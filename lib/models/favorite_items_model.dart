9e7ba98640707c0a099812a1d1ae048144f8a896
bath0ry <plylegacy@gmail.com>
2023-05-05 20:56:55 -0300

    organize app in mvc pattern

--- lib/models/favorite_items_model.dart
+++ lib/models/favorite_items_model.dart
@@ -1,4 +1,4 @@
-import 'package:commerce_flutter/data/models/store_model.dart';
+import 'package:commerce_flutter/models/store_model.dart';
 import 'package:flutter/material.dart';
 
 class FavItems extends ChangeNotifier {
--- test/unity_test.dart
+++ test/unity_test.dart
@@ -1,5 +1,6 @@
-import 'package:commerce_flutter/data/models/items_model.dart';
-import 'package:commerce_flutter/data/models/store_model.dart';
+import 'package:commerce_flutter/models/models/items_model.dart';
+import 'package:commerce_flutter/models/store_model.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
